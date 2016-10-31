ee73fbb799d006fec0efd0a10725bc9fa19916cd
Scott Myers <scott.myers622@gmail.com>
2016-10-31 18:50:28 -0500

    removes single-line comments, but not multi-line

--- comment-remover.c
+++ comment-remover.c
@@ -25,7 +25,12 @@
  *
  * Main Function
  * - reads input char by char until EOF
- * - 
+ * - prints input with getchar() and putchar()
+ * - if boundary of comment is detected, stop printing
+ *   - int boundary, incomment
+ *   - boundary -> switch between 0 and 1 when '/' is encountered
+ *   - incomment -> switch between 0 and 1 when '*' is encountered && boundary == 1
+ * - resume printing when boundary == 0 && incomment == 0
  *
  */
 
@@ -36,14 +41,31 @@
 int main()
 {
     int c;
-    int boundary = incomment = 0;
+    int boundary = 0;
+    int incomment = 0;
 
     while ((c = getchar()) != EOF)
     {
-        if (c == '/')
+        /* entering comment... */
+        if (c == '/' && incomment == 0)
             boundary = 1;
+        else if (c == '*' && boundary == 1)
+            incomment = 1;
 
-        else if (incomment == 0)
+        /* exiting comment... */
+        else if (c == '/' && incomment == 1)
+            boundary = 0;
+        else if ((c != '/' && c != '*') && boundary == 0)
+        {
             putchar(c);
+            incomment = 0;
+        }
+
+        /* not in a comment */
+        else if (incomment == 0)
+        {
+            putchar(c);
+            boundary = 0;
+        }
     }
 }
