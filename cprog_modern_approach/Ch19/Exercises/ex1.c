ee9a071d5fa5ef98dc2d62bc8a9eeaf8eaf30c44
scottmurs <scott.myers622@gmail.com>
2017-12-24 02:29:33 -0600

    working on chapter 22 project 8

---
+++ cprog_modern_approach/Ch19/Exercises/ex1.c
@@ -0,0 +1,11 @@
+// queue.h (FIFO)
+
+// insert item at end of queue
+
+// remove item from beginning of queue
+
+// return first item in queue
+
+// return last item in queue
+
+// test whether queue is empty
