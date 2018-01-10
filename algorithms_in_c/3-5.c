1471e4d1549b61e8e7f625ce0cb64f97ee441626
scottmurs <scott.myers622@gmail.com>
2018-01-10 17:07:25 -0600

    doubly linked list implementation

--- algorithms_in_c/3-5.c
+++ algorithms_in_c/3-5.c
@@ -25,39 +25,58 @@ struct node {
     struct node *prev;
 };
 
-struct node *make_node(int d);
-void insert(struct node *p);
-void delete(struct node *p);
-struct node *find_node(int d);
 void clean_up(struct node *p);
+void del_node(struct node *p);
+struct node *find_node(int d);
+void ins_node(struct node *p);
+struct node *make_node(int d);
+void print_node_list(struct node *p);
 
 struct node *head, *tail;
 
-int main(void)
+void clean_up(struct node *node_ptr)
 {
-    struct node *p;
+    struct node *prev_node;
 
-    head->next = tail;
-    head->prev = NULL;
-    tail->next = tail;
-    tail->prev = head;
-    
-    // create list
-    for (int i = 0; i < 10; i++) {
-        p = make_node(i);
-        printf("made a node...\n");
-        insert(p);
-        printf("inserted a node...\n");
+    while(node_ptr != tail) {
+        prev_node = node_ptr;
+        node_ptr = node_ptr->next;
+        del_node(prev_node);
     }
-    // insert a few nodes
-    //
-    // find a particular node
-    //
-    // delete it
-    //
-    // clean up
-    //
-    // exit
+    del_node(tail);
+}
+
+void del_node(struct node *p)
+{
+    p->next->prev = p->prev;
+    p->prev->next = p->next;
+    free(p);
+}
+
+struct node *find_node(int d)
+{
+    struct node *node_ptr;
+
+    node_ptr = head->next;
+    while(node_ptr->data != d)
+        node_ptr = node_ptr->next;
+
+    if (node_ptr == tail) {
+        fprintf(stderr, "node not found");
+        return NULL;
+    }
+
+    return node_ptr;
+}
+
+void insert(struct node *p)
+{
+    struct node *old_end;
+    old_end = tail->prev;
+    tail->prev = p;
+    p->next = tail;
+    p->prev = old_end;
+    old_end->next = p;
 }
 
 struct node *make_node(int d)
@@ -74,47 +93,41 @@ struct node *make_node(int d)
     return node_ptr;
 }
 
-void insert(struct node *p)
+void print_list(struct node *p)
 {
-    struct node *old_end;
-    old_end = tail->prev;
-    tail->prev = p;
-    p->next = tail;
-    p->prev = old_end;
-    old_end->next = p;
+  // assumes head is passed to print_list
+  p = p->next;
+  while (p != tail) {
+    printf("%d ", p->data);
+    p = p->next;
+  }
+  printf("\n");
 }
 
-void delete(struct node *p)
+int main(void)
 {
-    free(p);
-}
+    struct node *p;
+    
+    head = malloc(sizeof(*head));
+    tail = malloc(sizeof(*tail));
 
-struct node *find_node(int d)
-{
-    struct node *node_ptr;
-
-    node_ptr = head->next;
-    while(node_ptr->data != d);
-        node_ptr = node_ptr->next;
-
-    if (node_ptr == tail) {
-        fprintf(stderr, "node not found");
-        return NULL;
+    head->next = tail;
+    head->prev = head;
+    tail->next = tail;
+    tail->prev = head;
+    
+    // create list
+    for (int i = 0; i < 10; i++) {
+        p = make_node(i);
+        insert(p);
     }
-
-    return node_ptr;
+    
+    print_list(head);
+    
+    // delete one
+    struct node *to_del = find_node(3);
+    if (to_del != NULL)
+      del_node(to_del);
+    
+    print_list(head);
 }
-
-void clean_up(struct node *node_ptr)
-{
-    struct node *prev_node;
-
-    while(node_ptr != tail) {
-        prev_node = node_ptr;
-        node_ptr = node_ptr->next;
-        free(prev_node);
-    }
-    delete(tail);
-}
-
-
